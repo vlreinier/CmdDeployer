a2891988a0b07ce9c2e18f9cbefb1526c41c71f1
vlreinier <43336873+vlreinier@users.noreply.github.com>
2021-08-09 16:04:50 +0200

    Logs and icon

--- CmdDeployer.py
+++ CmdDeployer.py
@@ -4,10 +4,13 @@ import Settings
 import os
 
 if __name__ == "__main__":
-    if os.path.exists(Settings.logs):
-        os.remove(Settings.logs)
+    if not os.path.exists(Settings.logdir):
+        os.mkdir(Settings.logdir)
+    logfile = os.path.join(Settings.logdir, os.environ['COMPUTERNAME'] + ".log")
+    if os.path.exists(logfile):
+        os.remove(logfile)
     logging.basicConfig(
-        filename=Settings.logs,
+        filename=logfile,
         filemode='a',
         format='%(asctime)s %(levelname)s %(message)s',
         datefmt='%H:%M:%S',
@@ -21,10 +24,10 @@ if __name__ == "__main__":
     screenheight = root.winfo_screenheight()
     width = Settings.width_perc * screenwidth
     height = Settings.height_perc * screenheight
-    root.geometry("{:0.0f}x{:0.0f}+{:0.0f}+{:0.0f}".format(width, height,
-                    (screenwidth / 2) - (width / 2), (screenheight / 2) - (height / 2)))
-    root.minsize(Settings.min_width, Settings.min_height)
     root.lift()
     root.attributes('-topmost',True)
     root.after_idle(root.attributes,'-topmost',False)
+    root.geometry("{:0.0f}x{:0.0f}+{:0.0f}+{:0.0f}".format(width, height,
+                    (screenwidth / 2) - (width / 2), (screenheight / 2) - (height / 2)))
+    root.minsize(Settings.min_width, Settings.min_height)
     root.mainloop()
